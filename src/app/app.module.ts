65c89d94a802ffc8957a5d188d0c603120773c40
Jader Neira <jader.neira@gmail.com>
2020-06-26 21:57:35 -0500

    ++ Pokemon APIService Description

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PokemonsComponent } from './components/pokemons/pokemons.component';
+
+import {HttpClientModule} from '@angular/common/http';
+import { PokeDescriptionComponent } from './components/poke-description/poke-description.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    PokemonsComponent,
+    PokeDescriptionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/pokemons/pokemons.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { PokeAPIService } from '../../services/poke-api.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-pokemons',
+  templateUrl: './pokemons.component.html',
+  styleUrls: ['./pokemons.component.scss']
+})
+export class PokemonsComponent implements OnInit {
+  Pokemons:any = [];
+
+  constructor(
+    private PokeService: PokeAPIService,
+    private route: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.getAllPokemons();
+  }
+
+  getAllPokemons(){
+    this.PokeService.getPokemons().subscribe(pokemons => {
+      console.log(pokemons);
+      this.Pokemons = pokemons;
+    })
+  }
+
+  showPoke(id){
+    this.route.navigateByUrl(`pokemons/${id}`)    
+  }
+
+}
